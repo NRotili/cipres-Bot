a50c487038cd46bda4ed02f20cd0de48a37e2fa8
NRotili <rotilinicolas@gmail.com>
2024-09-06 18:23:09 -0300

    act. 2024-09-06

--- base-ts-baileys-memory/src/flows/final.flow.ts
+++ base-ts-baileys-memory/src/flows/final.flow.ts
@@ -1,9 +1,11 @@
 import { addKeyword, EVENTS } from "@builderbot/bot";
+import { stop } from "~/utils/idle-custom";
 
 const finalFlow = addKeyword(EVENTS.ACTION)
     .addAnswer(["Perfecto, espero haberte ayudado. 😁", "Cualquier otra cosa que necesites, aquí estaré. 😊"], {delay:2000})
     .addAction(async (ctx, ctxFn) => {
-        return ctxFn.endFlow("Hasta luego! 👋")
+        stop(ctx);
+        return ctxFn.endFlow("Hasta luego! 👋");
     });
 
 export { finalFlow };
